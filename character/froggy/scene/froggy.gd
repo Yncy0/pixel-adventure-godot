8e1b4e27b3ddc76dd60222df56d4060390874738
Yncy0 <paalayancy@gmail.com>
2025-05-15 10:07:23 +0800

    refactor a little bit

--- character/froggy/scene/froggy.gd
+++ character/froggy/scene/froggy.gd
@@ -16,11 +16,11 @@ func _physics_process(delta: float) -> void:
 		sprite.play("jump")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
