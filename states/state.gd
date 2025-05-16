a26c5ed0849abd8f28a24c3ea389d4ec5c32305f
Yncy0 <paalayancy@gmail.com>
2025-05-16 09:46:10 +0800

    added: state machine

---
+++ states/state.gd
@@ -0,0 +1,15 @@
+class_name State extends Node
+
+signal change_state(new_state_name: StringName)
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func update_process(_delta: float) -> void:
+	pass
+
+func update_physics(_delta: float) -> void:
+	pass
---
+++ states/state_machine.gd
@@ -0,0 +1,35 @@
+class_name StateMachine extends Node
+
+@export var current_state: State
+
+var states: Dictionary = {}
+
+func _ready() -> void:
+	for i in get_children():
+		if i is State:
+			states[i.name.to_lower()] = i
+			i.change_state.connect(next_state)
+		else:
+			printerr("There is no Nodes existing")
+	
+	await owner.ready
+	current_state.enter()
+
+func _process(delta: float) -> void:
+	current_state.update_process(delta)
+
+func _physics_process(delta: float) -> void:
+	current_state.update_physics(delta)
+
+func next_state(new_state_name: StringName) -> void:
+	var new_state = states.get(new_state_name.to_lower())
+	
+	if new_state != null:
+		if new_state != current_state:
+			current_state.exit()
+			new_state.enter()
+			current_state = new_state
+		else:
+			printerr("Nodes doesn't match the next state %s" % new_state)
+	else:
+		printerr("The new state is null")
