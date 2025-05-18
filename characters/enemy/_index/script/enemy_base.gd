39fa65d841671184d8a3283abe1d269442e8f599
Yncy0 <paalayancy@gmail.com>
2025-05-18 23:32:57 +0800

    added: chicken enemy chase

--- characters/enemy/_index/script/enemy_base.gd
+++ characters/enemy/_index/script/enemy_base.gd
@@ -1,9 +1,10 @@
 class_name EnemyBase extends CharacterBody2D
 
-
+@export var _Player: Player
 @export var Speed = 300.0
 @export var Jump_Velocity = -400.0
 
+@onready var area_player_detect: Area2D = $AreaPlayerDetect
 
-func _physics_process(delta: float) -> void:
-	move_and_slide()
+
+var is_player: bool = false
--- characters/enemy/chicken/script/chicken.gd
+++ characters/enemy/chicken/script/chicken.gd
@@ -1,1 +1,22 @@
 class_name Chicken extends EnemyBase
+
+
+func _physics_process(delta: float) -> void:
+	if !is_player:
+		velocity.x = move_toward(velocity.x, 0.0, Speed)
+	if is_player:
+		var dir = (_Player.global_position - global_position).normalized()
+		velocity.x = dir.x * Speed
+	
+	move_and_slide()
+
+
+func _on_area_player_detect_body_entered(body: Node2D) -> void:
+	if body is Player:
+		is_player = true
+
+
+
+func _on_area_player_detect_body_exited(body: Node2D) -> void:
+	if body is Player:
+		is_player = false
