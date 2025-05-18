c31cb4a800f47b09d490ef1657a7d62d7d06827a
Yncy0 <paalayancy@gmail.com>
2025-05-18 23:49:27 +0800

    finished: chicken basic actions

--- characters/enemy/_index/script/enemy_base.gd
+++ characters/enemy/_index/script/enemy_base.gd
@@ -5,6 +5,7 @@ class_name EnemyBase extends CharacterBody2D
 @export var Jump_Velocity = -400.0
 
 @onready var area_player_detect: Area2D = $AreaPlayerDetect
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 var is_player: bool = false
--- characters/enemy/chicken/script/chicken.gd
+++ characters/enemy/chicken/script/chicken.gd
@@ -4,9 +4,19 @@ class_name Chicken extends EnemyBase
 func _physics_process(_delta: float) -> void:
 	if !is_player:
 		velocity.x = move_toward(velocity.x, 0.0, Speed)
-	if is_player:
+	elif is_player:
 		var dir = (_Player.global_position - global_position).normalized()
 		velocity.x = dir.x * Speed
+	
+	if velocity.x > 0:
+		sprite.flip_h = true
+	elif velocity.x < 0:
+		sprite.flip_h = false
+	
+	if velocity.x == 0:
+		sprite.play("idle")
+	elif velocity.x != 0:
+		sprite.play("run")
 	
 	move_and_slide()
 
