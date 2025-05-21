c74be3e593b83ec8b95d875aec43c5af4b7db101
Yncy0 <paalayancy@gmail.com>
2025-05-21 12:30:01 +0800

    finished: falling platform

---
+++ world/props/traps/falling_platforms/script/falling_platform.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
+
+
+var is_falling: bool = false
+
+
+func _physics_process(delta: float) -> void:
+	if is_falling:
+		velocity.y += 980 / 2 * delta
+		animated_sprite_2d.play("off")
+		
+	
+	if is_on_floor():
+		self.queue_free()
+	
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		is_falling = true
+		timer.start(3)
+
+
+func _on_timer_timeout() -> void:
+	timer.stop()
+	self.queue_free()
